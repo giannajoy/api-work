a15fc753b26651daf13a1cb0cdf6eea9b8d6d060
giannajoy <33935380+giannajoy@users.noreply.github.com>
2019-05-30 16:59:05 +0300

    Add files via upload

---
+++ screencaster-3b.php
@@ -0,0 +1,12 @@
+<?php
+    function stringconverter () {
+
+        $screencast = [
+            $names => " 'Wairuri Kamau', 'Kambua Wamvua', 'Topoi Susan', 'Wafula Jotham'";
+            $profession => " 'Engineer', 'Doctor', 'Miss', 'Mister' ";
+            $number => " '20', '15', '6', '12', '4', '1', '2', '3', '7' ";
+            $char => " '-', '|', '#', '£', '%', '_', '.', ',', '+'";
+        ]
+    }
+
+?>
